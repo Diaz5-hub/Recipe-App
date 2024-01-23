65de44edab79a62af4789368603d64a097aa2792
Alexander Diaz <AlexDiaz6022000@gmail.com>
2024-01-23 15:43:29 -0800

    added endpoints for registering. Do login next

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";//API
-import cors from 'cors';    //set up rules between comm between front and beckend
-import mongoose from 'mongoose';//to send comm and queries to our DB
+import cors from "cors";    //set up rules between comm between front and beckend
+import mongoose from "mongoose";//to send comm and queries to our DB
+import {userRouter} from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);   //write endpoints asoociated with authentication for testing
+
+mongoose.connect("");
 
 app.listen(3001, () => console.log("Server started"));
