d10d551b0b42f7a309f69b79058a3f56990e9660
Alexander Diaz <AlexDiaz6022000@gmail.com>
2024-01-23 17:20:42 -0800

    added functionality for login and register on the backend. Now start the UI for recipes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -19,7 +19,20 @@ router.post("/register", async (req,res) => {
     res.json({message: "User Registered Successfully"});
 });
 
-router.post("/login");
+router.post("/login", async(req,res) => {
+    const {username, password} = req.body;
+    const user = await UserModel.findOne({username});
+    if(!user){
+        return res.json({message: "User doesn't exist!"});
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        return res.json({message: "username or password is incorrect! "});
+    }
+    //logging in with the correct information
+    const token = jwt.sign({id: user._id},"secret");
+    res.json({token, userID: user._id});
+});
 
 
 
